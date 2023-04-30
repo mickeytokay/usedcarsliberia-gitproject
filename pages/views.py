aaecf1d191faea59b43d8fa839c0c55b0b22c3b3
Mickey Tokay <goldeninkmultimedia@gmail.com>
2023-04-30 08:39:53 +0000

    created pages and configured navigation links

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'pages/home.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+def login(request):
+    return render(request, 'pages/login.html')
+
+def register(request):
+    return render(request, 'pages/register.html')
