2b1068954f6822cc9cbda9a2f027db29a77b3ae6
Mickey Tokay <goldeninkmultimedia@gmail.com>
2023-05-07 09:55:01 +0000

    completed search functionalities and admin customization

--- cars/views.py
+++ cars/views.py
@@ -9,8 +9,16 @@ def cars(request):
     paginator = Paginator(cars, 4)
     page = request.GET.get('page')
     paged_cars = paginator.get_page(page)
+    model_search = Car.objects.values_list('model', flat=True).distinct() 
+    county_search = Car.objects.values_list('county', flat=True).distinct() 
+    year_search = Car.objects.values_list('year', flat=True).distinct() 
+    body_type_search = Car.objects.values_list('body_type', flat=True).distinct()
     data = {
         'cars': paged_cars,
+        'model_search': model_search,
+        'county_search': county_search,
+        'year_search': year_search,
+        'body_type_search': body_type_search,
     }
     return render(request, 'cars/cars.html', data)
 
@@ -23,4 +31,48 @@ def car_detail(request, id):
     return render(request, 'cars/car_detail.html', data)
 
 def search(request):
-    return render(request, 'cars/search.html')
+    cars = Car.objects.order_by('-created_date')
+    model_search = Car.objects.values_list('model', flat=True).distinct() 
+    county_search = Car.objects.values_list('county', flat=True).distinct() 
+    year_search = Car.objects.values_list('year', flat=True).distinct() 
+    body_type_search = Car.objects.values_list('body_type', flat=True).distinct()
+    
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            cars = cars.filter(description__icontains=keyword)
+
+    if 'model' in request.GET:
+        model = request.GET['model']
+        if model:
+            cars = cars.filter(model__iexact=model)
+
+    if 'county' in request.GET:
+        county = request.GET['county']
+        if county:
+            cars = cars.filter(county__iexact=county)
+
+    if 'year' in request.GET:
+        year = request.GET['year']
+        if year:
+            cars = cars.filter(year__iexact=year)
+
+    if 'body_type' in request.GET:
+        body_type = request.GET['body_type']
+        if body_type:
+            cars = cars.filter(body_type__iexact=body_type)
+
+    if 'min_price' in request.GET:
+        min_price = request.GET['min_price']
+        max_price = request.GET['max_price']
+        if max_price:
+            cars = cars.filter(price__gte=min_price, price__lte=max_price)
+
+    data = {
+        'cars': cars,
+        'model_search': model_search,
+        'county_search': county_search,
+        'year_search': year_search,
+        'body_type_search': body_type_search,
+    }
+    return render(request, 'cars/search.html', data)
--- pages/views.py
+++ pages/views.py
@@ -7,10 +7,18 @@ def home(request):
     teams = Team.objects.all()
     cars = Car.objects.order_by('-created_date')
     featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
+    model_search = Car.objects.values_list('model', flat=True).distinct() 
+    county_search = Car.objects.values_list('county', flat=True).distinct() 
+    year_search = Car.objects.values_list('year', flat=True).distinct() 
+    body_type_search = Car.objects.values_list('body_type', flat=True).distinct() 
     data = {
         'teams': teams,
         'cars': cars,
         'featured_cars': featured_cars,
+        'model_search': model_search,
+        'county_search': county_search,
+        'year_search': year_search,
+        'body_type_search': body_type_search,
     }
     return render(request, 'pages/home.html', data)
 
